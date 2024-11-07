8d44f1b4c63a852900ee95dee417c2151221ff98
Alessandra <alessandra.projex@gmail.com>
2024-11-07 15:21:56 -0400

    ajustando validação de login

--- src/homePage/login/Login.jsx
+++ src/homePage/login/Login.jsx
@@ -5,8 +5,63 @@ import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import AppleLogin from 'react-apple-login';
 import iconApple from '../../assets/svg/iconApple.svg';
 import iconGoogle from '../../assets/svg/iconGoogle.svg';
+import { useRef, useState, useEffect, useContext } from 'react';
+// import { use } from '../../../back-july/routes/users';
+import AuthContext from '../../context/authProvider';
+import axios from '../../api/axios';
+
+const LOGIN_URL = './auth';
 
 function Login() {
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMessage, setErrMessage] = useState('');
+    const [success, setSucess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMessage('');
+    }, [email, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(LOGIN_URL, 
+                JSON.stringify({email, password}),
+                {
+                    headers: {'Content-type': 'application/json'},
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data.roles;
+            setAuth({email, password, roles, accessToken});
+            setEmail('');
+            setPassword('');
+            setSucess(true); 
+        }
+        catch (error) {
+            if(!error?.response) {
+                setErrMessage('No server response');
+            } else if (error.response?.status === 400 ) {
+                setErrMessage('Missing Email or Password');
+            } else if (error.response?.status === 401) {
+                setErrMessage('Unauthorized');
+            } else {
+                setErrMessage('Login failed')
+            }
+            errRef.current.focus();
+        }  
+    }
 
     const handleGoogleLoginSuccess = (response) => {
         console.log("Google login successful:", response);
@@ -27,58 +82,79 @@ function Login() {
     };
 
     return (
-        <GoogleOAuthProvider clientId="SUA_CLIENT_ID_GOOGLE">
-            <div className='container-register'>
-                <div className='form'>
-                    <h2>Bem vindo de volta</h2>
-                    <p className='text-entrar'>Entrar em sua conta</p>
-                    <form>
-                        <div className='input'>
-                            <label>Email</label>
-                            <input type="email" name="email" placeholder='E-mail' />
-                        </div>
-                        <div className='input'>
-                            <label>Senha</label>
-                            <input type="password" name="password" placeholder='Senha' />
-                        </div>
-                        <button className='button-register' type="submit">Entrar</button>
-                        <p className='text-center p-5 pb-2'>ou</p>
-                        
-                        <div className='button-icons'>
-                            <GoogleLogin
-                                onSuccess={handleGoogleLoginSuccess}
-                                onError={handleGoogleLoginFailure}
-                                render={(renderProps) => (
-                                    <button onClick={renderProps.onClick}>
-                                        <img src={iconGoogle} alt="Google Icon" /> Entrar com Google
-                                    </button>
-                                )}
-                            />
-                            <AppleLogin
-                                clientId="SUA_CLIENT_ID_APPLE"
-                                redirectURI="SUA_REDIRECT_URI"
-                                onSuccess={handleAppleLoginSuccess}
-                                onFailure={handleAppleLoginFailure}
-                                render={(renderProps) => (
-                                    <button onClick={renderProps.onClick}>
-                                        <img src={iconApple} alt="Apple Icon" /> Entrar com Apple
-                                    </button>
-                                )}
-                            />
-                        </div>
+        // <GoogleOAuthProvider clientId="SUA_CLIENT_ID_GOOGLE">
+        <>
+        {success ? (
+            <section>
+                <h1>You are logged in</h1>
+                <br/>
+                <p>
+                    <a href='#'>Go to Home</a>
+                </p>
+            </section>
 
-                        <p className='text-center'>
-                            Não tem uma conta? <Link className='link' to='/register'>Cadastrar</Link>
-                        </p>
-                    </form>
-                </div>
+        ) : (
 
-                {/* <div className='imageRegister'> */}
-                    <img className='imageRegister' src={imageRegister} alt="Register" />
-                {/* </div> */}
+        <section>
+            <p ref={errRef} className={errMessage ? "errmsg" : "offscreen"} aria-live='assertive'>{errMessage}</p><div className='container-register'>
+            <div className='form'>
+                <h2>Bem vindo de volta</h2>
+                <p className='text-entrar'>Entrar em sua conta</p>
+                <form onSubmit={handleSubmit}>
+                    <div className='input'>
+                        <label htmlFor='email'>Email</label>
+                        <input id='email' ref={userRef} type="email" name="email" placeholder='E-mail'
+                            value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    </div>
+                    <div className='input'>
+                        <label htmlFor='password'>Senha</label>
+                        <input id='password' type="password" name="password" placeholder='Senha'
+                            value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    </div>
+                    <button className='button-register' type="submit">Entrar</button>
+                    <p className='text-center p-5 pb-2'>ou</p>
+
+                    {/* <div className='button-icons'>
+        <GoogleLogin
+            onSuccess={handleGoogleLoginSuccess}
+            onError={handleGoogleLoginFailure}
+            render={(renderProps) => (
+                <button onClick={renderProps.onClick}>
+                    <img src={iconGoogle} alt="Google Icon" /> Entrar com Google
+                </button>
+            )}
+        />
+        <AppleLogin
+            clientId="SUA_CLIENT_ID_APPLE"
+            redirectURI="SUA_REDIRECT_URI"
+            onSuccess={handleAppleLoginSuccess}
+            onFailure={handleAppleLoginFailure}
+            render={(renderProps) => (
+                <button onClick={renderProps.onClick}>
+                    <img src={iconApple} alt="Apple Icon" /> Entrar com Apple
+                </button>
+            )}
+        />
+    </div> */}
+
+                    <p className='text-center'>
+                        Não tem uma conta? <Link className='link' to='/register'>Cadastrar</Link>
+                    </p>
+                </form>
             </div>
-        </GoogleOAuthProvider>
+
+            {/* <div className='imageRegister'> */}
+            <img className='imageRegister' src={imageRegister} alt="Register" />
+            {/* </div> */}
+        </div>
+    
+        </section>
+        )}
+
+        {/* // </GoogleOAuthProvider> */}
+</>
     );
+    
 }
 
 export default Login;
