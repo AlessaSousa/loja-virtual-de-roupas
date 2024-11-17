45edb66e249c48de449b3b32af73f9d845491f7d
Gabriel Takashi Gadelha Yamashita <gabrieltgyamashita@gmail.com>
2024-11-17 20:10:15 -0300

    catalogo/filtro

---
+++ src/homePage/filter/Filter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Filter.css";
+
+const Filter = ({ categories, onFilterChange }) => {
+  return (
+    <div className="filter-container">
+      <select 
+        onChange={(e) => onFilterChange(e.target.value)} 
+        className="filter-dropdown"
+        aria-label="Filtrar por categoria"
+      >
+        <option value="">Todas as categorias</option>
+        {categories.map((category, index) => (
+          <option key={index} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
--- src/homePage/itensCatalago/ItensCatalago.jsx
+++ src/homePage/itensCatalago/ItensCatalago.jsx
@@ -1,78 +1,54 @@
-import calcaAlfaiataria from '../../assets/svg/calça-alfaiataria.svg'
-import camisaMasculina from '../../assets/svg/camisa-masculina-branca.svg'
-import sobretudoBege from '../../assets/svg/sobretudo-bege.svg'
-import Slider from "react-slick";
-import './ItensCatalago.css'
+import React from 'react';
+import calcaAlfaiataria from '../../assets/svg/calça-alfaiataria.svg';
+import camisaMasculina from '../../assets/svg/camisa-masculina-branca.svg';
+import sobretudoBege from '../../assets/svg/sobretudo-bege.svg';
+import camisaXadrez from '../../assets/png/camisa-xadrez.png';
+import JaquetaTreino from '../../assets/png/jaqueta-treino.png';
+import CamisaInfantil from '../../assets/png/camiseta-infantil.png';
+import BlusaFeminina from '../../assets/png/blusa-feminina.png';
+import CalçaJeans from '../../assets/png/calça-jeans.png';
+import ShortsCorrida from '../../assets/png/shorts-corrida.png';
+import Saia from '../../assets/png/saia.png';
+import TopEsportivo from '../../assets/png/top-esportivo.png';
+import JaquetaCouro from '../../assets/png/jaqueta-couro.png';
+import './ItensCatalago.css';
 import { Link } from 'react-router-dom';
 
-function ItensCatalago() {
+function ItensCatalago({ selectedCategory }) {
+  const roupas = [
+    { img: calcaAlfaiataria, title: 'Conjunto paletó e calça', value: 'R$ 45.00', category: 'Calças' },
+    { img: camisaMasculina, title: 'Camisa oversize branca', value: 'R$ 45.00', category: 'Camisas' },
+    { img: sobretudoBege, title: 'Sobretudo bege', value: 'R$ 45.00', category: 'Sobretudos' },
+    { img: camisaXadrez, title: "Camisa xadrez clássica", value: "R$ 49.90", category: "Masculino" },
+    { img: JaquetaTreino, title: "Jaqueta de treino", value: "R$ 199.90", category: "Esporte" },
+    { img: CamisaInfantil, title: "Camiseta Infantil", value: "R$ 29.90", category: "Infantil" },
+    { img: BlusaFeminina, title: "Blusa feminina", value: "R$ 59.90", category: "Feminino" },
+    { img: CalçaJeans, title: "Calça jeans", value: "R$ 89.90", category: "Masculino" },
+    { img: ShortsCorrida, title: "Shorts de corrida", value: "R$ 49.90", category: "Esporte" },
+    { img: Saia, title: "Saia Jeans", value: "R$ 69.90", category: "Feminino" },
+    { img: TopEsportivo, title: "Conjunto Esportivo", value: "R$ 139.90", category: "Esporte" },
+    { img: JaquetaCouro, title: "Jaqueta de couro", value: "R$ 129.90", category: "Feminino" },
+  ];
 
-    const roupas = [
-        {
-            img: calcaAlfaiataria,
-            title: 'Conjunto paletó e calça',
-            value: 'R$ 45.00'
-        },
-        {
-            img: camisaMasculina,
-            title: 'Camisa oversize branca',
-            value: 'R$ 45.00'
-        },
-        {
-            img: sobretudoBege,
-            title: 'Sobretudo bege',
-            value: 'R$ 45.00'
-        },
-        {
-            img: calcaAlfaiataria,
-            title: 'Conjunto paletó e calça',
-            value: 'R$ 45.00'
-        },
-        {
-            img: camisaMasculina,
-            title: 'Camisa oversize branca',
-            value: 'R$ 45.00'
-        },
-        {
-            img: sobretudoBege,
-            title: 'Sobretuto bege',
-            value: 'R$ 45.00'
-        },
+  // Filtra os itens com base na categoria selecionada
+  const filteredRoupas = selectedCategory ? roupas.filter(item => item.category === selectedCategory) : roupas;
 
-    ]
-    const sliderConfig = {
-        centerMode: false,
-        centerPadding: '20px',
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        infinite: true,
-        dots: false,
-        arrows: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-    }
-
-    return (
-        <>
-            <div className='container-cards'>
-                <Slider {...sliderConfig}>
-
-                    {roupas.map((image, index) =>
-                        <div key={index}  className='cards'>
-                            <img className='image-cards' src={image.img}></img>
-                            <p className='text'>{image.title}</p>
-                            <div className='value-button'>
-                                <p className='text'>{image.value}</p>
-                                <button className="button-buy">
-                                    <Link>Comprar</Link>
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                </Slider>
-            </div>
-        </>
-    )
+  return (
+    <div className="container-cards">
+      {filteredRoupas.map((image, index) =>
+        <div key={index} className='cards'>
+          <img className='image-cards' src={image.img} alt={image.title} />
+          <p className='text'>{image.title}</p>
+          <div className='value-button'>
+            <p className='text'>{image.value}</p>
+            <button className="button-buy">
+              <Link to="#">Comprar</Link>
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-export default ItensCatalago
+
+export default ItensCatalago;
