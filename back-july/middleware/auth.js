c452d2455ebe6f970a13a83f4a678c644a7879ea
july.silva <july081503@gmail.com>
2024-12-05 16:43:08 -0400

    adição de autenticação por JWT

---
+++ back-july/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const secret = process.env.JWT_SECRET
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startswith('Bearer')){
+        return res.status(401).send({error: 'Acesso negado. Token não encontrado'})
+    }
+
+    const token = authHeader.split(' ')[1]
+    try {
+        const decoded = jwt.verify(token, secret)
+        req.user = decoded
+        next()
+    } catch (error) {
+        return res.status(403).send({error: 'Token inválido'})
+    }
+}
+
+module.exports = verifyToken
